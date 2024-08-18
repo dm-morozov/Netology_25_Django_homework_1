6b2b8b0099ed3a98e6bd2536b303afce1399517f
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2024-08-18 12:36:23 +0500

    Problem solved: 2.1-databases | Решена задача: 2.1-databases

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,9 +2,12 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
+    help = 'Импорт данных телефона из файла CSV'
+
     def add_arguments(self, parser):
         pass
 
@@ -14,4 +17,14 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            phone_instance = Phone(
+                name = phone.get('name', 'Имя не указано'),
+                price = float(phone.get('price', '0')),
+                image = phone.get('image', 'Изображение не указано'),
+                release_date = phone.get('release_date', '01.01.0001'),
+                lte_exists = bool(phone.get('lte_exists', 'False')),
+                slug = slugify(phone.get('name', 'Имя не указано'))
+            )
+            phone_instance.save()
+
+        self.stdout.write(self.style.SUCCESS('Телефоны успешно импортированы!'))
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,36 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+    phones = Phone.objects.all()
+
+    if sort == 'name':
+        phones = phones.order_by('name')
+    elif sort == 'min_price':
+        phones = phones.order_by('price')
+    elif sort == 'max_price':
+        phones = phones.order_by('-price')
+
+     
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    # Строчка phone = get_object_or_404(Phone, slug=slug) в Django 
+    # используется для получения объекта модели Phone по полю slug 
+    # из базы данных, при отсутствии объекта возвращается ошибка 404.
+    # phone = get_object_or_404(Phone, slug=slug)
+
+    # Другой вариант решения через try-except:
+    try:
+        phone = Phone.objects.get(slug=slug)
+        print(vars(phone))
+    except Phone.DoesNotExist:
+        return redirect('catalog')
+    
+    context = {'phone': phone}
+    
     return render(request, template, context)
